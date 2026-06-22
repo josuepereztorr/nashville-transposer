b4bc8947b02e97e5690817c0f66dceae718d8d69
josuepereztorr <josuepereztorr@gmail.com>
2026-06-21 21:54:56 -0500

    added Note struct and addded placeholder funcitons

---
+++ theory.c
@@ -0,0 +1,16 @@
+typedef struct
+{
+    int message;
+    int status;
+    char note[2];
+    int velocity;
+} Note;
+
+int get_nashville_num(Note note)
+{
+    return 1;
+}
+
+int get_note_name(Note note)
+{
+}
