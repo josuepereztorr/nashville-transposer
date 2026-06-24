4faf162308707df4b6946fb0a93b1eac836f50bb
josuepereztorr <josuepereztorr@gmail.com>
2026-06-23 21:10:19 -0500

    added midi_notes.csv and created read_cvs() to read the file

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "midi.c"
+#include "theory.c"
 
 int main()
 {
-    midi_start();
+    // midi_start();
+    read_cvs();
     return 0;
 };
 
--- theory.c
+++ theory.c
@@ -1,16 +1,56 @@
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_NUM_OF_NOTES 128
+#define MAX_BUFFER_LINE_LENGTH 50
+
 typedef struct
 {
-    int message;
     int status;
-    char note[2];
-    int velocity;
+    int note;
+    char name[2];
+    int octave;
 } Note;
 
-int get_nashville_num(Note note)
+static Note keyboard[MAX_NUM_OF_NOTES] = {0};
+static int notes_per_scale = 12;
+
+void create_keyboard()
 {
-    return 1;
 }
 
-int get_note_name(Note note)
+// Reads the midi_notes.csv file
+int read_cvs()
 {
+    // OS provides a handle for us to read the file.
+    FILE *file = fopen("midi_notes.csv", "r");
+
+    // pointer will be NULL if the file can't be opened ex. wrong path, no permissions, file doesn't exit.
+    if (file == NULL)
+    {
+        // TODO - get more information on using erno & perror
+        return -1;
+    }
+
+    /* holds one line of characters (MAX_BUFFER_LINE_LENGTH - 1),
+       including the control characters (or non-printing characters)
+       "\n" line feed
+       "\r" carriage return
+       "\0" null character */
+    char buffer[MAX_BUFFER_LINE_LENGTH] = {0};
+
+    // buffer - is used to hold temporary data (one line from csv)
+    // returns a pointer to the buffer or NULL if an error occurs or if the End of File (EOF) has reached.
+    while (fgets(buffer, sizeof(buffer), file) != NULL)
+    {
+        // what to do with the data
+        // parse data by removing the control characters
+        char *data = strtok(buffer, ",");
+        printf("Note: %s\n", data);
+    }
+
+    // NULL case
+
+    fclose(file);
+    return 0;
 }
