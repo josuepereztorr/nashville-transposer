1bb06643e43b3f622ef269495d2053a7eab00aa4
josuepereztorr <josuepereztorr@gmail.com>
2026-07-06 20:38:36 -0500

    refactored ui.c

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -22,7 +22,7 @@ typedef struct
 
 typedef struct
 {
-    UIElement element;
+    UIElement el;
     int is_pressed;
 } UINote;
 
@@ -30,31 +30,34 @@ typedef struct
 int num_of_keys = 25;
 
 UIElement create_ui_element(int left_margin, double top_margin, int parent_width, int parent_height, Color color);
-void create_white_note(int x_offset, int y_offset, int width, int height, Color color, int num);
+UINote create_wht_note_with_multiplier(int x_offset, int y_offset, int width, int height, int num);
+UINote create_wht_note(int parent_x_offset, int parent_y_offset, int parent_width, int parent_height);
+UINote create_blk_note(int parent_x_offset, int parent_y_offset, int parent_width, int parent_height);
+void create_octave();
 
 void ui_create_keyboard()
 {
-
-    // Parent - background
-    // UIElement ui1 = create_ui_element(20, 0.025, GetScreenWidth() * 0.25, (GetScreenHeight() * 0.625), WHITE);
-    // UIElement ui2 = create_ui_element(ui1.width + 20, 0.025, ui1.width * 2, (GetScreenHeight() * 0.625), BLUE);
-    // UIElement ui3 = create_ui_element(ui2.width + 20, 0.025, GetScreenWidth() * 0.25, (GetScreenHeight() * 0.625), GREEN);
 
     int bg_x_offset = 20;
     int bg_y_offset = GetScreenHeight() * 0.625;
     int bg_width = GetScreenWidth() - (bg_x_offset * 2);
     int bg_height = (GetScreenHeight() * 0.375) - 20;
-    DrawRectangle(bg_x_offset, bg_y_offset, bg_width, bg_height, foreground);
+    DrawRectangle(bg_x_offset, bg_y_offset, bg_width, bg_height, failure_midi);
 
-    for (int i = 0; i < num_of_keys; i++)
-    {
-        create_white_note(bg_x_offset, bg_y_offset, bg_width, bg_height, primary_text, i);
-    }
+    // for (int i = 0; i < num_of_keys; i++)
+    //{
+    // UINote wht_note = create_wht_note_with_multiplier(bg_x_offset, bg_y_offset, bg_width, bg_height, 0);
+    //}
 
-    // UI black key
+    UINote wht_note = create_wht_note(bg_x_offset, bg_y_offset, bg_width, bg_height);
+    UINote blk_note = create_blk_note(wht_note.el.x_offset, wht_note.el.y_offset, wht_note.el.width, wht_note.el.height);
 }
 
-void create_white_note(int parent_x_offset, int parent_y_offset, int parent_width, int parent_height, Color color, int multiplier)
+void create_octave()
+{
+}
+
+UINote create_wht_note_with_multiplier(int parent_x_offset, int parent_y_offset, int parent_width, int parent_height, int multiplier)
 {
     // space between keys
     int key_padding = 2;
@@ -70,7 +73,67 @@ void create_white_note(int parent_x_offset, int parent_y_offset, int parent_widt
 
     x_offset = parent_x_offset + multiplier;
 
-    DrawRectangle(x_offset, parent_y_offset, width, parent_height, color);
+    UINote wht_note = {};
+    wht_note.el.x_offset = x_offset;
+    wht_note.el.y_offset = parent_y_offset;
+    wht_note.el.width = width;
+    wht_note.el.height = parent_height;
+    wht_note.el.color = primary_text;
+
+    DrawRectangle(wht_note.el.x_offset,
+                  wht_note.el.y_offset,
+                  wht_note.el.width,
+                  wht_note.el.height,
+                  wht_note.el.color);
+
+    // return wht_note;
+
+    UINote blk_note = create_blk_note(wht_note.el.x_offset, wht_note.el.y_offset, wht_note.el.width, wht_note.el.height);
+
+    return wht_note;
+}
+
+UINote create_wht_note(int parent_x_offset, int parent_y_offset, int parent_width, int parent_height)
+{
+    UINote wht_note = {};
+    wht_note.el.x_offset = parent_x_offset;
+    wht_note.el.y_offset = parent_y_offset;
+    wht_note.el.width = parent_width / num_of_keys;
+    wht_note.el.height = parent_height;
+    wht_note.el.color = primary_text;
+
+    DrawRectangle(wht_note.el.x_offset,
+                  wht_note.el.y_offset,
+                  wht_note.el.width,
+                  wht_note.el.height,
+                  wht_note.el.color);
+
+    UINote blk_note = create_blk_note(wht_note.el.x_offset, wht_note.el.y_offset, wht_note.el.width, wht_note.el.height);
+
+    return wht_note;
+}
+
+UINote create_blk_note(int parent_x_offset, int parent_y_offset, int parent_width, int parent_height)
+{
+    // white to black note ratios
+    const float width_ratio = 1.71;
+    const float height_ratio = 1.5;
+    const float offset_ratio = 0.75;
+
+    UINote blk_note = {};
+    blk_note.el.x_offset = parent_x_offset + (parent_width * offset_ratio);
+    blk_note.el.y_offset = parent_y_offset;
+    blk_note.el.width = parent_width / width_ratio;
+    blk_note.el.height = parent_height / height_ratio;
+    blk_note.el.color = border;
+
+    DrawRectangle(blk_note.el.x_offset,
+                  blk_note.el.y_offset,
+                  blk_note.el.width,
+                  blk_note.el.height,
+                  blk_note.el.color);
+
+    return blk_note;
 }
 
 // top_margin is a value from 0.00 to 1.00
