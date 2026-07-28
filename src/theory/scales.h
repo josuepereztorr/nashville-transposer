64d132f5805e8f379f88bba1688fee7b6150d88e
josuepereztorr <josuepereztorr@gmail.com>
2026-07-28 17:41:17 -0500

    created  scales.h

---
+++ src/theory/scales.h
@@ -0,0 +1,185 @@
+#ifndef SCALES_H
+#define SCALES_H
+
+static const int scale_c[12] = {
+    1, // C
+    0, // C#
+    1, // D
+    0, // D#
+    1, // E
+    1, // F
+    0, // F#
+    1, // G
+    0, // G#
+    1, // A
+    0, // A#
+    1  // B
+};
+
+static const int scale_c_sharp[12] = {
+    1, // C
+    1, // C#
+    0, // D
+    1, // D#
+    0, // E
+    1, // F
+    1, // F#
+    0, // G
+    1, // G#
+    0, // A
+    1, // A#
+    0  // B
+};
+
+static const int scale_d[12] = {
+    0, // C
+    1, // C#
+    1, // D
+    0, // D#
+    1, // E
+    0, // F
+    1, // F#
+    1, // G
+    0, // G#
+    1, // A
+    0, // A#
+    1  // B
+};
+
+// Eb major: C, D, D#, F, G, G#, A#
+static const int scale_d_sharp[12] = {
+    1, // C
+    0, // C#
+    1, // D
+    1, // D#
+    0, // E
+    1, // F
+    0, // F#
+    1, // G
+    1, // G#
+    0, // A
+    1, // A#
+    0  // B
+};
+
+static const int scale_e[12] = {
+    0, // C
+    1, // C#
+    0, // D
+    1, // D#
+    1, // E
+    0, // F
+    1, // F#
+    0, // G
+    1, // G#
+    1, // A
+    0, // A#
+    1  // B
+};
+
+static const int scale_f[12] = {
+    1, // C
+    0, // C#
+    1, // D
+    0, // D#
+    1, // E
+    1, // F
+    0, // F#
+    1, // G
+    0, // G#
+    1, // A
+    1, // A#
+    0  // B
+};
+
+static const int scale_f_sharp[12] = {
+    0, // C
+    1, // C#
+    0, // D
+    1, // D#
+    0, // E
+    1, // F
+    1, // F#
+    0, // G
+    1, // G#
+    0, // A
+    1, // A#
+    1  // B
+};
+
+static const int scale_g[12] = {
+    1, // C
+    0, // C#
+    1, // D
+    0, // D#
+    1, // E
+    0, // F
+    1, // F#
+    1, // G
+    0, // G#
+    1, // A
+    0, // A#
+    1  // B
+};
+
+static const int scale_g_sharp[12] = {
+    1, // C
+    1, // C#
+    0, // D
+    1, // D#
+    0, // E
+    1, // F
+    0, // F#
+    1, // G
+    1, // G#
+    0, // A
+    1, // A#
+    0  // B
+};
+
+static const int scale_a[12] = {
+    0, // C
+    1, // C#
+    1, // D
+    0, // D#
+    1, // E
+    0, // F
+    1, // F#
+    0, // G
+    1, // G#
+    1, // A
+    0, // A#
+    1  // B
+};
+
+static const int scale_a_sharp[12] = {
+    1, // C
+    0, // C#
+    1, // D
+    1, // D#
+    0, // E
+    1, // F
+    0, // F#
+    1, // G
+    0, // G#
+    1, // A
+    1, // A#
+    0  // B
+};
+
+static const int scale_b[12] = {
+    0, // C
+    1, // C#
+    0, // D
+    1, // D#
+    1, // E
+    0, // F
+    1, // F#
+    0, // G
+    1, // G#
+    0, // A
+    1, // A#
+    1  // B
+};
+
+#endif
